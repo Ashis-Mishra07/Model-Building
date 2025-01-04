4425f87e2cebe160eea5dffd68f0f8840001cdff
Ashis Kumar Mishra <137470813+Ashis-Mishra07@users.noreply.github.com>
2025-01-04 19:26:26 +0530

    Add files via upload

---
+++ _7_logistic_regression_multiclass.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_digits
+# datasets contains 8x8 images of faded image and I have to guess the number written on it 
+
