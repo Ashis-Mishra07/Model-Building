807475682329ab4c7a7a7021d3181ba8d1baa448
Ashis Kumar Mishra <137470813+Ashis-Mishra07@users.noreply.github.com>
2025-01-04 09:36:23 +0530

    Update _2_linear_regression_multiple_variable.py
    updated the linear reg model

---
+++ _2_linear_regression_multiple_variable.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+
+price = m1*area + m2*bedrooms + m3*age + b
+
+# How to handle the NaN valuea and Linear Regre using multiple variables
+
+
+# filling the bedroom section
+
+import math
+median_bedrooms = math.floor(df.bedrooms.median())
+
+df.bedrooms = df.bedrooms.fillna(median_bedrooms)
+
+reg = linear_model.LinearRegression()
+reg.fit(df[['area','bedrooms','age']],df.price)
+
+# to know about the coefficients m1 m2 m3 and intercept b
+reg.coef_
+reg.intercept_
+
+# now how to predict
+reg.predict([[3000,3,40]])
