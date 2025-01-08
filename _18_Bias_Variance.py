8b319aeb929bd92ca2c556fd5ce3ae3483549992
Ashis Kumar Mishra <137470813+Ashis-Mishra07@users.noreply.github.com>
2025-01-08 15:55:15 +0530

    Add files via upload

---
+++ _18_Bias_Variance.py
@@ -0,0 +1,27 @@
+'''
+
+Bias vs Variance
+
+for low fit model or overfit model(High curve) , the training error is very low infact it is 0 . 
+but test error varies with very high if we choose differ overfit strategies . This is high variance .
+since train error is low , hence called low bias .
+
+
+for high fit model or underfit model(straight line) , traing error will be significant . so high bias .
+And the test error will also be significant , but do not varies much , hence low variance .
+
+
+Bias -> think about train error 
+Variance -> think about test error
+
+
+for Balanced fit model the bias and variance are both low .
+
+How to get Balanced fit model ?
+1. Cross Validation
+2. Regularization
+3 . Dimensionality Reduction (PCA)
+4. Ensemble Methods (Bagging , Boosting)
+
+
+'''
